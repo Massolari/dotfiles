99efee2ef301e2000e814c8f24525cfbc8e0f27a
Douglas Massolari <douglasmassolari@hotmail.com>
2021-09-14 10:25:24 -0300

    Nvim - Ajuste no mapeamento de snippets
    - Adicionado .editorconfig

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -19,37 +19,37 @@ return require('packer').startup(function(use)
   -- Sessões
   use 'xolox/vim-misc'
   use {
-      'xolox/vim-session',
-      requires = { 'xolox/vim-misc' },
-      opt = true,
-      cmd = {'SaveSession', 'OpenSession', 'DeleteSession', 'CloseSession'}
+    'xolox/vim-session',
+    requires = { 'xolox/vim-misc' },
+    opt = true,
+    cmd = {'SaveSession', 'OpenSession', 'DeleteSession', 'CloseSession'}
   }
 
   -- Buscador
   use { 'junegunn/fzf', run = './install --bin', }
   use {
-      'ibhagwan/fzf-lua',
-      requires = {
-          'vijaymarupudi/nvim-fzf',
-          'kyazdani42/nvim-web-devicons'
-      },
-      config = function ()
-          require'fzf-lua'.setup({ fzf_layout = 'default' })
-      end
+    'ibhagwan/fzf-lua',
+    requires = {
+      'vijaymarupudi/nvim-fzf',
+      'kyazdani42/nvim-web-devicons'
+    },
+    config = function ()
+      require'fzf-lua'.setup({ fzf_layout = 'default' })
+    end
   }
 
   -- Sintasse para várias linguagens
   use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      config = function ()
-          require'nvim-treesitter.configs'.setup {
-              ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-              highlight = {
-                  enable = true,              -- false will disable the whole extension
-              },
-          }
-      end
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function ()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        highlight = {
+          enable = true,              -- false will disable the whole extension
+        },
+      }
+    end
   }
 
   -- Exibe espaços vazios no final da linha
@@ -57,39 +57,37 @@ return require('packer').startup(function(use)
 
   -- Adicionar comentários com 'gcc'
   use {
-      'tpope/vim-commentary',
-      opt = true,
-      keys = 'gc',
+    'tpope/vim-commentary',
+    opt = true,
+    keys = 'gc',
   }
 
   -- Habilita o uso do emmet (<C-y>,)
   use {
-      'mattn/emmet-vim',
-      opt = true,
-      keys = { '<c-y>,' },
+    'mattn/emmet-vim',
   }
 
   -- Habilita a busca rapida usando duas letras
   use {
-      'ggandor/lightspeed.nvim',
-      config = function ()
-          require'lightspeed'.setup {
-              jump_to_first_match = true,
-              jump_on_partial_input_safety_timeout = 400,
-              highlight_unique_chars = false,
-              grey_out_search_area = true,
-              match_only_the_start_of_same_char_seqs = true,
-              limit_ft_matches = 5,
-              full_inclusive_prefix_key = '<c-x>',
-          }
-          for _, key in ipairs({'f', 'F', 't', 'T'}) do
-              vim.api.nvim_del_keymap('o', key)
-              vim.api.nvim_del_keymap('x', key)
-              vim.api.nvim_del_keymap('n', key)
-          end
-      end,
-      opt = true,
-      keys = { 'S', 's' }
+    'ggandor/lightspeed.nvim',
+    config = function ()
+      require'lightspeed'.setup {
+        jump_to_first_match = true,
+        jump_on_partial_input_safety_timeout = 400,
+        highlight_unique_chars = false,
+        grey_out_search_area = true,
+        match_only_the_start_of_same_char_seqs = true,
+        limit_ft_matches = 5,
+        full_inclusive_prefix_key = '<c-x>',
+      }
+      for _, key in ipairs({'f', 'F', 't', 'T'}) do
+        vim.api.nvim_del_keymap('o', key)
+        vim.api.nvim_del_keymap('x', key)
+        vim.api.nvim_del_keymap('n', key)
+      end
+    end,
+    opt = true,
+    keys = { 'S', 's' }
   }
 
   -- Temas
@@ -109,39 +107,39 @@ return require('packer').startup(function(use)
   use {
     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require'gitsigns'.setup {
-            numhl = false,
-            linehl = false,
-            keymaps = {
-                -- Default keymap options
-                noremap = true,
-                buffer = true,
+      require'gitsigns'.setup {
+        numhl = false,
+        linehl = false,
+        keymaps = {
+          -- Default keymap options
+          noremap = true,
+          buffer = true,
 
-                ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-                ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+          ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
+          ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
 
-                ['n <leader>ghu'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-                ['v <leader>ghu'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-                ['n <leader>ghv'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+          ['n <leader>ghu'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+          ['v <leader>ghu'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+          ['n <leader>ghv'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
 
-                -- Text objects
-                -- ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-                -- ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-            },
-            current_line_blame = true,
-            current_line_blame_opts = {
-                delay = 0
-            }
+          -- Text objects
+          -- ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+          -- ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+        },
+        current_line_blame = true,
+        current_line_blame_opts = {
+          delay = 0
         }
+      }
     end
   }
 
   -- Mostra linhas de indentação
   use {
-      'lukas-reineke/indent-blankline.nvim',
-      config = function ()
-          require'indent_blankline'.setup()
-      end
+    'lukas-reineke/indent-blankline.nvim',
+    config = function ()
+      require'indent_blankline'.setup()
+    end
   }
 
   -- Auto-fechamento de delimitadores
@@ -158,33 +156,38 @@ return require('packer').startup(function(use)
 
   -- Explorador de arquivos
   use {
-      'kyazdani42/nvim-tree.lua',
-      requires = 'kyazdani42/nvim-web-devicons',
-      opt = true,
-      cmd = {'NvimTreeToggle', 'NvimTreeFindFile'}
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    opt = true,
+    cmd = {'NvimTreeToggle', 'NvimTreeFindFile'}
   }
 
   -- Warper para comandos do git
   use {
-      'tpope/vim-fugitive',
-      opt = true,
-      cmd = {'Git', 'Gdiff', 'Gclog', 'Gwrite'}
+    'tpope/vim-fugitive',
+    opt = true,
+    cmd = {'Git', 'Gdiff', 'Gclog', 'Gwrite'}
   }
 
   -- Engine de snippets
-  use 'SirVer/ultisnips'
+  use {
+    'SirVer/ultisnips',
+    config = function()
+      vim.g.UltiSnipsExpandTrigger = "<c-y>"
+    end
+  }
 
   -- Biblioteca de snippets
   use 'honza/vim-snippets'
 
   -- Status line
   use {
-      'glepnir/galaxyline.nvim',
-      branch = 'main',
-      -- your statusline
-      config = function() require'eviline' end,
-      -- some optional icons
-      requires = {'kyazdani42/nvim-web-devicons'}
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    -- your statusline
+    config = function() require'eviline' end,
+    -- some optional icons
+    requires = {'kyazdani42/nvim-web-devicons'}
   }
 
   -- Guia de atalhos
@@ -192,9 +195,9 @@ return require('packer').startup(function(use)
 
   -- Cliente REST
   use {
-      'diepm/vim-rest-console',
-      opt = true,
-      ft = 'rest',
+    'diepm/vim-rest-console',
+    opt = true,
+    ft = 'rest',
   }
 
   -- LSP do Nvim
@@ -203,131 +206,140 @@ return require('packer').startup(function(use)
 
   -- Autocompletion framework for built-in LSP
   use {
-      "hrsh7th/nvim-cmp",
-      requires = {
-          "hrsh7th/vim-vsnip",
-          "quangnguyen30192/cmp-nvim-ultisnips",
-          "hrsh7th/cmp-nvim-lsp"
-          -- "hrsh7th/cmp-buffer",
-      },
-      config = function ()
-          local cmp = require'cmp'
-          local lspkind = require'lspkind'
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/vim-vsnip",
+      "quangnguyen30192/cmp-nvim-ultisnips",
+      "hrsh7th/cmp-nvim-lsp"
+      -- "hrsh7th/cmp-buffer",
+    },
+    config = function ()
+      local cmp = require'cmp'
+      local lspkind = require'lspkind'
 
-          cmp.setup({
-              snippet = {
-                  expand = function(args)
-                      vim.fn["UltiSnips#Anon"](args.body)
-                  end,
-              },
-              mapping = {
-                  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ['<C-e>'] = cmp.mapping.close(),
-              },
-              formatting = {
-                  format = function(entry, vim_item)
-                      vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-                      vim_item.menu = ({
-                          path = "[Path]",
-                          buffer = "[Buffer]",
-                          calc = "[Calc]",
-                          nvim_lsp = "[LSP]",
-                          cmp_tabnine = "[TabNine]",
-                          ultisnips = "[UltiSnips]",
-                          emoji = "[Emoji]",
-                      })[entry.source.name]
-                      return vim_item
-                  end
-              },
-              sources = {
-                  { name = 'path' },
-                  { name = 'buffer' },
-                  { name = 'calc' },
-                  { name = 'nvim_lsp' },
-                  { name = 'cmp_tabnine' },
-                  { name = 'ultisnips' },
-                  { name = 'emoji' },
-              }
-          })
-      end,
+      local t = function(str)
+        return vim.api.nvim_replace_termcodes(str, true, true, true)
+      end
+
+      local check_back_space = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body)
+          end,
+        },
+        mapping = {
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+            vim_item.menu = ({
+              path = "[Path]",
+              buffer = "[Buffer]",
+              calc = "[Calc]",
+              nvim_lsp = "[LSP]",
+              cmp_tabnine = "[TabNine]",
+              ultisnips = "[UltiSnips]",
+              emoji = "[Emoji]",
+            })[entry.source.name]
+            return vim_item
+          end
+        },
+        sources = {
+          { name = 'path' },
+          { name = 'buffer' },
+          { name = 'calc' },
+          { name = 'nvim_lsp' },
+          { name = 'cmp_tabnine' },
+          { name = 'ultisnips' },
+          { name = 'emoji' },
+        }
+      })
+    end,
   }
 
   -- TabNine
-    use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
 
   -- Plugin LSP com base no cliente lsp do neovim
   use {
-      'glepnir/lspsaga.nvim',
-      requires = {'neovim/nvim-lspconfig'},
-      config = function ()
-          require 'lspsaga'.init_lsp_saga()
-      end,
-      opt = true,
-      module = 'lspsaga',
+    'glepnir/lspsaga.nvim',
+    requires = {'neovim/nvim-lspconfig'},
+    config = function ()
+      require 'lspsaga'.init_lsp_saga()
+    end,
+    opt = true,
+    module = 'lspsaga',
   }
 
   -- Assinaturas de funções ao digitar
   use {
-      'ray-x/lsp_signature.nvim',
-      config = function ()
-          require'lsp_signature'.setup()
-      end
+    'ray-x/lsp_signature.nvim',
+    config = function ()
+      require'lsp_signature'.setup()
+    end
   }
 
 
   -- Informações de LSP na statusline
   use {
-      'nvim-lua/lsp-status.nvim',
-      config = function ()
-          require'lsp-status'.register_progress()
-      end
+    'nvim-lua/lsp-status.nvim',
+    config = function ()
+      require'lsp-status'.register_progress()
+    end
   }
 
   -- Ícones no completion
   use {
-      'onsails/lspkind-nvim',
-      config = function ()
-          require'lspkind'.init()
-      end
+    'onsails/lspkind-nvim',
+    config = function ()
+      require'lspkind'.init()
+    end
   }
 
   -- Alternador de terminal
   use {
-      'akinsho/nvim-toggleterm.lua',
-      config = function ()
-          require'toggleterm'.setup{
-              open_mapping = [[<c-\>]],
-              shade_terminals = false,
-              direction = 'horizontal'
-          }
-      end,
-      opt = true,
-      cmd = 'ToggleTerm',
+    'akinsho/nvim-toggleterm.lua',
+    config = function ()
+      require'toggleterm'.setup{
+        open_mapping = [[<c-\>]],
+        shade_terminals = false,
+        direction = 'horizontal'
+      }
+    end,
+    opt = true,
+    cmd = 'ToggleTerm',
   }
 
   -- Lista de diagnostics
   use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-          require'trouble'.setup {
-              auto_preview = false,
-              signs = {
-                  -- icons / text used for a diagnostic
-                  error = " - Err - ",
-                  warning = " - War - ",
-                  hint = " - Hin - ",
-                  information = " - Inf - "
-              },
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end,
-      opt = true,
-      cmd = 'Trouble',
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require'trouble'.setup {
+        auto_preview = false,
+        signs = {
+          -- icons / text used for a diagnostic
+          error = " - Err - ",
+          warning = " - War - ",
+          hint = " - Hin - ",
+          information = " - Inf - "
+        },
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    opt = true,
+    cmd = 'Trouble',
   }
 
   -- Mostrar o que há nos registradores
@@ -335,10 +347,10 @@ return require('packer').startup(function(use)
 
   -- Buffers no topo
   use {
-      'akinsho/bufferline.nvim',
-      requires = 'kyazdani42/nvim-web-devicons',
-      config = function()
-          require'bufferline'.setup()
-      end
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require'bufferline'.setup()
+    end
   }
 end)
