7727a62631f96e70c63aa4e75c8b0867eb3aab03
Douglas Massolari <douglasmassolari@hotmail.com>
2021-10-04 16:23:06 -0300

    Nvim - Definição de tema ajustada
    - Retirado require do tema do github

--- config/nvim/lua/settings/globals.lua
+++ config/nvim/lua/settings/globals.lua
@@ -2,24 +2,21 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
--- -- Gruvbox
+-- Gruvbox
 vim.g.gruvbox_italic = 1
 
--- -- Material
+-- Material
 vim.g.material_style = 'lighter'
 
--- -- Neon
+-- Neon
 vim.g.neon_style = 'light'
 vim.g.neon_bold = true
 
--- -- Tokyonight
+-- Tokyonight
 vim.g.tokyonight_style = 'day'
 
--- vim.cmd('colorscheme github_light')
-require'github-theme'.setup({
-  theme_style = 'light',
-  -- dark_float = true,
-  comment_style = 'italic',
-  function_style = 'italic',
-  keyword_style = 'italic'
-})
+-- Github
+vim.g.function_style = 'italic'
+
+-- Colorscheme
+vim.cmd('colorscheme github_light')
