e1af3af91663d291b5ca8a91ac041f24fe90467c
Douglas M <douglasmassolari@hotmail.com>
2021-11-25 13:14:28 -0300

    Nvim - Habilitado codelens do lsp

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -44,27 +44,46 @@ local on_attach = function(client, bufnr)
   -- Set autocommands conditional on server_capabilities
   if client.resolved_capabilities.document_highlight then
     if vim.opt.background:get() ~= 'light' then
-      vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=black guibg=black
-      hi LspReferenceText cterm=bold ctermbg=black guibg=black
-      hi LspReferenceWrite cterm=bold ctermbg=black guibg=black
-        ]], false)
+      vim.api.nvim_exec(
+        [[
+        hi LspReferenceRead cterm=bold ctermbg=black guibg=black
+        hi LspReferenceText cterm=bold ctermbg=black guibg=black
+        hi LspReferenceWrite cterm=bold ctermbg=black guibg=black
+        ]],
+        false)
     else
-      vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=lightyellow guibg=lightyellow
-      hi LspReferenceText cterm=bold ctermbg=lightyellow guibg=lightyellow
-      hi LspReferenceWrite cterm=bold ctermbg=lightyellow guibg=lightyellow
-        ]], false)
+      vim.api.nvim_exec(
+        [[
+        hi LspReferenceRead cterm=bold ctermbg=lightyellow guibg=lightyellow
+        hi LspReferenceText cterm=bold ctermbg=lightyellow guibg=lightyellow
+        hi LspReferenceWrite cterm=bold ctermbg=lightyellow guibg=lightyellow
+        ]],
+        false)
     end
-    vim.api.nvim_exec([[
-    augroup lsp_document_highlight
-    autocmd! * <buffer>
-    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-      ]], false)
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+      autocmd! * <buffer>
+      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+      ]],
+      false)
+
   end
 
+  if client.resolved_capabilities.code_lens then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_code_lens_refresh
+      autocmd! * <buffer>
+      autocmd InsertLeave <buffer> lua vim.lsp.codelens.refresh()
+      autocmd InsertLeave <buffer> lua vim.lsp.codelens.display()
+      augroup END
+      ]],
+      false
+    )
+  end
   vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border})
   vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border})
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
