454f1fb1b21c73267d6757dbb693736dc83236c2
Douglas M <douglasmassolari@hotmail.com>
2021-11-29 17:14:31 -0300

    Nvim - Ajuste nos diagnostics exibidos bufferline

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -290,9 +290,14 @@ return require'packer'.startup(function(use)
       require'bufferline'.setup{
         options = {
           diagnostics = 'nvim_lsp',
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icon = level:match("error") and " " or " "
-            return " " .. icon .. count
+          diagnostics_indicator = function(_, _, diagnostics_dict, _)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+              local sym = e == "error" and " "
+                or (e == "warning" and " " or "" )
+              s = s .. n .. sym
+            end
+            return s
           end
         }
       }
