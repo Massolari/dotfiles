ae0700cb80c56eeedba0977940a7f47f6c0e9eab
Douglas M <douglasmassolari@hotmail.com>
2021-11-29 17:11:30 -0300

    Nvim - Diagnostics sendo mostrados na bufferline

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -287,7 +287,15 @@ return require'packer'.startup(function(use)
     'akinsho/bufferline.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
     config = function()
-      require'bufferline'.setup()
+      require'bufferline'.setup{
+        options = {
+          diagnostics = 'nvim_lsp',
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end
+        }
+      }
     end
   }
 
