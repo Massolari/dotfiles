a068055bc367c0b760cb7f1ec40f54b71364841f
Douglas M <douglasmassolari@hotmail.com>
2023-05-16 09:48:52 +0200

    feat(zsh): definir variáveis de locale (LC_TYPE e LC_ALL)

--- flake.nix
+++ flake.nix
@@ -259,6 +259,8 @@
                 enable = true;
                 enableAutosuggestions = true;
                 initExtraFirst = ''
+                  export LC_TYPE=pt_BR.UTF-8
+                  export LC_ALL=pt_BR.UTF-8
                   # Fig pre block. Keep at the top of this file.
                   . "$HOME/.fig/shell/zshrc.pre.zsh"
                 '';
