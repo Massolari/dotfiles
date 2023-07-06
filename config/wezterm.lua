ef1919cb69fcceeae4e4a1bb4760839f5c473750
Douglas M <douglasmassolari@hotmail.com>
2023-07-06 12:51:31 +0200

    refactor(wezterm): formatação do arquivo de configuração

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,35 +1,37 @@
 local
-
 act = wezterm.action
-local io = require "io"
-local os = require "os"
+local io = require("io")
+local os = require("os")
 
 wezterm.on("trigger-nvim-with-scrollback", function(window, pane)
   -- Retrieve the current viewport's text.
   -- Pass an optional number of lines (eg: 2000) to retrieve
   -- that number of lines starting from the bottom of the viewport
-  local scrollback = pane:get_lines_as_text(2000);
+  local scrollback = pane:get_lines_as_text(2000)
 
   -- Create a temporary file to pass to vim
-  local name = os.tmpname();
-  local f = io.open(name, "w+");
-  f:write(scrollback);
-  f:flush();
-  f:close();
+  local name = os.tmpname()
+  local f = io.open(name, "w+")
+  f:write(scrollback)
+  f:flush()
+  f:close()
 
   -- Open a new window running vim and tell it to open the file
-  window:perform_action(wezterm.action {
-    SpawnCommandInNewWindow = {
-      args = { os.getenv('HOME') .. "/.nix-profile/bin/nvim", name, '-c', 'norm G' }
-    }
-  }, pane)
+  window:perform_action(
+    wezterm.action({
+      SpawnCommandInNewWindow = {
+        args = { os.getenv("HOME") .. "/.nix-profile/bin/nvim", name, "-c", "norm G" },
+      },
+    }),
+    pane
+  )
 
   -- wait "enough" time for vim to read the file before we remove it.
   -- The window creation and process spawn are asynchronous
   -- wrt. running this script and are not awaitable, so we just pick
   -- a number.
-  wezterm.sleep_ms(1000);
-  os.remove(name);
+  wezterm.sleep_ms(1000)
+  os.remove(name)
 end)
 
 wezterm.on("gui-startup", function(cmd)
@@ -62,105 +64,105 @@ return {
         fg_color = "#24292e",
         intensity = "Bold",
       },
-    }
+    },
   },
-  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font = wezterm.font("Iosevka Nerd Font"),
   hide_tab_bar_if_only_one_tab = true,
   keys = {
     {
-      key = 'T',
-      mods = 'CMD',
-      action = act.SpawnTab 'DefaultDomain',
+      key = "T",
+      mods = "CMD",
+      action = act.SpawnTab("DefaultDomain"),
     },
     {
-      key = 't',
-      mods = 'CMD',
-      action = act.SpawnCommandInNewTab {
-        label = 'Open new tab in home folder',
-        cwd = wezterm.home_dir
-      },
+      key = "t",
+      mods = "CMD",
+      action = act.SpawnCommandInNewTab({
+        label = "Open new tab in home folder",
+        cwd = wezterm.home_dir,
+      }),
     },
     {
-      key = 'Enter',
-      mods = 'CMD',
-      action = act.SplitPane {
-        direction = 'Down',
-      },
+      key = "Enter",
+      mods = "CMD",
+      action = act.SplitPane({
+        direction = "Down",
+      }),
     },
     {
-      key = 'Enter',
-      mods = 'CMD|SHIFT',
-      action = act.SplitPane {
-        direction = 'Right',
-      },
+      key = "Enter",
+      mods = "CMD|SHIFT",
+      action = act.SplitPane({
+        direction = "Right",
+      }),
     },
     {
-      key = 'r',
-      mods = 'CMD',
-      action = act.ActivateKeyTable {
-        name = 'resize_pane',
+      key = "r",
+      mods = "CMD",
+      action = act.ActivateKeyTable({
+        name = "resize_pane",
         one_shot = false,
-      },
+      }),
     },
     {
-      key = 'Z',
-      mods = 'CTRL',
+      key = "Z",
+      mods = "CTRL",
       action = act.TogglePaneZoomState,
     },
     {
-      key = ']',
-      mods = 'CMD',
-      action = act.ActivatePaneDirection 'Next',
+      key = "]",
+      mods = "CMD",
+      action = act.ActivatePaneDirection("Next"),
     },
     {
-      key = '[',
-      mods = 'CMD',
-      action = act.ActivatePaneDirection 'Prev',
+      key = "[",
+      mods = "CMD",
+      action = act.ActivatePaneDirection("Prev"),
     },
     {
-      key = 'J',
-      mods = 'CTRL',
+      key = "J",
+      mods = "CTRL",
       action = act.ActivateTabRelative(-1),
     },
     {
-      key = 'K',
-      mods = 'CTRL',
+      key = "K",
+      mods = "CTRL",
       action = act.ActivateTabRelative(1),
     },
     {
-      key = 'F',
-      mods = 'CTRL',
-      action = act.RotatePanes 'Clockwise',
+      key = "F",
+      mods = "CTRL",
+      action = act.RotatePanes("Clockwise"),
     },
     {
-      key = 'B',
-      mods = 'CTRL',
-      action = act.RotatePanes 'CounterClockwise',
+      key = "B",
+      mods = "CTRL",
+      action = act.RotatePanes("CounterClockwise"),
     },
     {
       key = "E",
       mods = "CTRL",
-      action = act { EmitEvent = "trigger-nvim-with-scrollback" }
+      action = act({ EmitEvent = "trigger-nvim-with-scrollback" }),
     },
     {
       key = "q",
       mods = "CTRL",
-      action = act { SendString="\x11" }
+      action = act({ SendString = "\x11" }),
     },
   },
   key_tables = {
     resize_pane = {
-      { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
-      { key = 'H', action = act.AdjustPaneSize { 'Left', 10 } },
-      { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
-      { key = 'L', action = act.AdjustPaneSize { 'Right', 10 } },
-      { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
-      { key = 'K', action = act.AdjustPaneSize { 'Up', 10 } },
-      { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
-      { key = 'J', action = act.AdjustPaneSize { 'Down', 10 } },
+      { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+      { key = "H", action = act.AdjustPaneSize({ "Left", 10 }) },
+      { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+      { key = "L", action = act.AdjustPaneSize({ "Right", 10 }) },
+      { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+      { key = "K", action = act.AdjustPaneSize({ "Up", 10 }) },
+      { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+      { key = "J", action = act.AdjustPaneSize({ "Down", 10 }) },
 
       -- Cancel the mode by pressing escape
-      { key = 'Enter', action = 'PopKeyTable' },
+      { key = "Enter", action = "PopKeyTable" },
     },
   },
   tab_bar_at_bottom = true,
