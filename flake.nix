85524afdcca14dcf09b72d5f8b98e7045dac103a
Douglas M. <douglasmassolari@hotmail.com>
2023-11-24 14:21:32 +0100

    feat(iamb): exibir nomes dos usuários

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
                 };
                 settings = {
                   image_preview = { };
+                  username_display = "displayname";
                 };
               };
 
