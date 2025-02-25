d7ecfa83dc8a0826381dcb159d10abce95af0085
Douglas M. <douglasmassolari@hotmail.com>
2025-02-25 15:52:41 +0100

    feat(fish): melhora configuração de key bindings vi
    Altera a configuração dos key bindings do fish shell para permitir a
    coexistência dos bindings vi e emacs no modo de inserção, mantendo a
    precedência do vi quando há conflito.
    
    Também define variáveis de ambiente como exportáveis usando -x.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,8 +13,19 @@ set PATH \
   /opt/homebrew/opt/openjdk/bin\
   $PATH
 
-set -g fish_key_bindings fish_vi_key_bindings
-set DYLD_LIBRARY_PATH "/opt/homebrew/lib"
-set XDG_CONFIG_HOME $HOME/.config
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
+
+set -g fish_key_bindings fish_user_key_bindings
+set -x DYLD_LIBRARY_PATH "/opt/homebrew/lib"
+set -x XDG_CONFIG_HOME $HOME/.config
 
 abbr --add lg lazygit
